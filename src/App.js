8aec24fb4ffaa6aebfe3e33ad535a2101081bc39
kayhals <kayhals4@gmail.com>
2023-07-18 17:33:51 -0500

    coding for GA6 completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import Buttons from "./components/Buttons";
-import Table from "./components/Table";
+import Buttons from "./components/Buttons.js";
+import Table from "./components/Table.js";
+import Banner from "./components/Banner.js";
+import {useState} from "react";
+import satData from "./components/satData.js";
 
-
-function App() {
+export default function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+  };
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat} />
     </div>
   );
 }
 
-export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import styling from "./styling.css";
+
+export default function Banner() {
+  return (
+    <>
+      <header>
+        <h1>Orbit Report</h1>
+      </header>
+      <p>Click one of the buttons to see the satellites in that orbit type.</p>
+    </>
+  );
+}
